5043e011f762f5afb9df68dae1224d049240fa4e
Tuguldur Bayartsolmon <togoldor.bayrtsolmon@gmail.com>
2024-05-13 14:24:37 +0800

    Update Terminal.js

--- isap/src/Terminal.js
+++ isap/src/Terminal.js
@@ -1,22 +1,27 @@
 import React, { useState } from "react";
 import Terminal, { ColorMode, TerminalOutput } from "react-terminal-ui";
 import "./Terminal.css";
+
 export default function TerminalController(props = {}) {
   const [terminalLineData, setTerminalLineData] = useState([
     <TerminalOutput>I SUCK AT PROGRAMMING 🤡</TerminalOutput>,
   ]);
+  const [colorMode, setColorMode] = useState(ColorMode.Dark);
 
   const handleInput = (terminalInput) => {
     const [command, ...args] = terminalInput.trim().split(" ");
     switch (command.toLowerCase()) {
-      case "customcommand":
-        handleCustomCommand(args);
-        break;
       case "help":
         handleHelpCommand();
         break;
       case "clear":
         handleClearCommand();
+        break;
+      case "light":
+        handleLightMode();
+        break;
+      case "dark":
+        handleDarkMode();
         break;
       default:
         setTerminalLineData((prevData) => [
@@ -29,34 +34,44 @@ export default function TerminalController(props = {}) {
     }
   };
 
-  const handleCustomCommand = (args) => {
-    setTerminalLineData((prevData) => [
-      ...prevData,
-      <TerminalOutput>
-        Custom command executed with arguments: {args.join(" ")}
-      </TerminalOutput>,
-    ]);
-  };
   const handleHelpCommand = () => {
     setTerminalLineData((prevData) => [
       ...prevData,
       <TerminalOutput>
         Available commands:
-        <br />- customcommand [args]: Execute a custom command with arguments
+        <br />- help: Show this help message
+        <br />- light: Switch to Light mode
+        <br />- dark: Switch to Dark mode
+        <br />- clear: Clear the terminal
       </TerminalOutput>,
     ]);
   };
+
   const handleClearCommand = () => {
     setTerminalLineData([]);
   };
-  const handleLightMode = () => {};
+
+  const handleLightMode = () => {
+    setColorMode(ColorMode.Light);
+    setTerminalLineData((prevData) => [
+      ...prevData,
+      <TerminalOutput>Switched to Light Mode</TerminalOutput>,
+    ]);
+  };
+  const handleDarkMode = () => {
+    setColorMode(ColorMode.Dark);
+    setTerminalLineData((prevData) => [
+      ...prevData,
+      <TerminalOutput>Switched to Dark Mode</TerminalOutput>,
+    ]);
+  };
 
   return (
     <Terminal
       name="kamaterasu@isap"
       prompt="kamaterasu@isap:~$"
       height="86vh"
-      colorMode={ColorMode.Dark}
+      colorMode={colorMode}
       onInput={handleInput}
     >
       {terminalLineData}
